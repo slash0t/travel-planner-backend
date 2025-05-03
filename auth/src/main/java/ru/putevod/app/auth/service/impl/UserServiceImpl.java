6121d8c2a9de9d957d33e4fbaf1713209068e2d7
dan00ile <nikelodeon53@gmail.com>
2025-05-03 23:47:34 +0300

    feat(user): temporary implement user service for editing email and login

---
+++ auth/src/main/java/ru/putevod/app/auth/service/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package ru.putevod.app.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.putevod.app.auth.dto.UserInfoDto;
+import ru.putevod.app.auth.model.User;
+import ru.putevod.app.auth.repository.UserRepository;
+import ru.putevod.app.auth.service.UserService;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional
+    public User updateLastLogin(User user) {
+        user.lastLogin(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserInfoDto mapToUserInfoDto(User user) {
+        return UserInfoDto.builder()
+                .id(user.userId())
+                .email(user.email())
+                .username(user.username())
+                .emailVerified(Boolean.TRUE.equals(user.isVerified()))
+                .createdAt(user.createdAt())
+                .build();
+    }
+} 
