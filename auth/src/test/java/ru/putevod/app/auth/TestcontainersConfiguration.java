256e32db18e28fc985c3880717978a3553d32157
dan00ile <nikelodeon53@gmail.com>
2025-05-03 23:31:03 +0300

    feat(auth): add application.properties for service

---
+++ auth/src/test/java/ru/putevod/app/auth/TestcontainersConfiguration.java
@@ -0,0 +1,11 @@
+package ru.putevod.app.auth;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+} 
