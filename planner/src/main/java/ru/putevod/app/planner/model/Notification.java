1bb68c0ae5eb5f9267009265608b531080a32759
dan00ile <nikelodeon53@gmail.com>
2025-05-04 10:56:28 +0300

    feat(models): implemented another 5 orm models according to database

---
+++ planner/src/main/java/ru/putevod/app/planner/model/Notification.java
@@ -0,0 +1,46 @@
+package ru.putevod.app.planner.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long notificationId;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    
+    @Column(nullable = false, length = 50)
+    private String type;
+    
+    @Column(nullable = false)
+    private String content;
+    
+    @Column(name = "related_id")
+    private Integer relatedId;
+    
+    @Column(name = "is_read")
+    private boolean read;
+    
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+} 
