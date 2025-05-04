9f2e1d913b6c45f699430acf1a36bfa4e7793e27
dan00ile <nikelodeon53@gmail.com>
2025-05-04 13:12:51 +0300

    feat(library): configuration file add with base settings, also add standart openApi and WebClient configs

---
+++ library/src/main/java/ru/putevod/app/library/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package ru.putevod.app.library.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Service API")
+                        .version("1.0")
+                        .description("API для сервиса публичных маршрутов TravelPlanner")
+                        .termsOfService("https://example.com/terms/")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact()
+                                .name("TravelPlanner Support")
+                                .email("support@travelplanner.example")
+                                .url("https://travelplanner.example")))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+} 
