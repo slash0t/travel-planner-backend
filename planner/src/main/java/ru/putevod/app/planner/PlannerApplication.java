c7155087a2e060291aafc171ce52ad342a432614
dan00ile <nikelodeon53@gmail.com>
2025-05-15 22:19:51 +0300

    fix(planner): change variables to environment ones  and add annotations to application

--- planner/src/main/java/ru/putevod/app/planner/PlannerApplication.java
+++ planner/src/main/java/ru/putevod/app/planner/PlannerApplication.java
@@ -1,12 +1,29 @@
 package ru.putevod.app.planner;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class PlannerApplication {
+@OpenAPIDefinition(info = @Info(
+        title = "Planner Service API",
+        version = "1.0",
+        description = "API для планирования маршрутов Putevod"
+))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class    PlannerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlannerApplication.class, args);
