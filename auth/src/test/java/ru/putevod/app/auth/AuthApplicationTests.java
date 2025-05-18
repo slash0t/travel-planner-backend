1d56c247d47f5db92d1199299660ca7e1b55f948
dan00ile <nikelodeon53@gmail.com>
2025-05-18 23:07:59 +0300

    fix: try another method to fix the problem with build

--- auth/src/test/java/ru/putevod/app/auth/AuthApplicationTests.java
+++ auth/src/test/java/ru/putevod/app/auth/AuthApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class AuthApplicationTests {
 
     @Test
