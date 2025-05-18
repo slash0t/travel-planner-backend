9781b260a81c4a68a3b2425d6556b4b890d02596
dan00ile <nikelodeon53@gmail.com>
2025-05-18 22:47:44 +0300

    fix: try another method to fix the problem with build

--- planner/src/test/java/ru/putevod/app/planner/TestcontainersConfiguration.java
+++ planner/src/test/java/ru/putevod/app/planner/TestcontainersConfiguration.java
@@ -9,13 +9,4 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-    
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
-                .withDatabaseName("testdb")
-                .withUsername("test")
-                .withPassword("test");
-    }
 }
