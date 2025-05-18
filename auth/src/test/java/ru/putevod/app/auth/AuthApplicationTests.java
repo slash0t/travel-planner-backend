227d2f8911511b708d92cc1d3a73229a239e511a
dan00ile <nikelodeon53@gmail.com>
2025-05-18 22:55:14 +0300

    fix: try another method to fix the problem with build

--- auth/src/test/java/ru/putevod/app/auth/AuthApplicationTests.java
+++ auth/src/test/java/ru/putevod/app/auth/AuthApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
-@Import(TestcontainersConfiguration.class)
 class AuthApplicationTests {
 
     @Test
