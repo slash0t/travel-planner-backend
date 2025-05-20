979d6e7adef75d8e8bd804def71bc057f42dc7b5
dan00ile <nikelodeon53@gmail.com>
2025-05-20 17:04:41 +0300

    feat(security): improve JWT error handling and validation details
    - Updated JwtAuthenticationFilter to catch and propagate exceptions

--- auth/src/main/java/ru/putevod/app/auth/security/JwtAuthenticationFilter.java
+++ auth/src/main/java/ru/putevod/app/auth/security/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -53,10 +55,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
+            
+            filterChain.doFilter(request, response);
         } catch (Exception ex) {
-            logger.error("Не удалось установить аутентификацию пользователя: " + ex.getMessage());
+            log.error("Ошибка аутентификации: {}", ex.getMessage());
+            throw ex;
         }
-
-        filterChain.doFilter(request, response);
     }
 } 
