c2d5eaebe71b54323fda7e0efe68fdef5e6a4b71
dan00ile <nikelodeon53@gmail.com>
2025-05-24 10:15:52 +0300

    fix(planner): fix tests

--- planner/src/test/java/ru/putevod/app/planner/TestcontainersConfiguration.java
+++ planner/src/test/java/ru/putevod/app/planner/TestcontainersConfiguration.java
@@ -1,11 +1,6 @@
 package ru.putevod.app.planner;
 
-import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
