d7795124e2573f51baf204e0d27abc1e7aadfc44
dan00ile <nikelodeon53@gmail.com>
2025-06-02 15:15:13 +0300

    fix(swagger): fix security config to access swagger

--- external/src/main/java/ru/putevod/app/external/config/SecurityConfig.java
+++ external/src/main/java/ru/putevod/app/external/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
             .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/v3/api-docs", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/api-docs-external/**").permitAll()
                 .requestMatchers("/swagger-ui", "/swagger-ui/", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/webjars/**", "/swagger-resources/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
