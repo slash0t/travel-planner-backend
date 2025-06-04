d0a4536f7a2f30812cf76fe792225a75d586ac56
dan00ile <nikelodeon53@gmail.com>
2025-06-04 13:01:01 +0300

    feat(planner): add migrations endpoints and service to planner

---
+++ planner/src/main/java/ru/putevod/app/planner/service/DataMigrationService.java
@@ -0,0 +1,46 @@
+package ru.putevod.app.planner.service;
+
+import ru.putevod.app.planner.model.TodoList;
+import ru.putevod.app.planner.model.Trip;
+
+import java.util.List;
+
+/**
+ * Сервис для миграции данных анонимного пользователя к зарегистрированному
+ */
+public interface DataMigrationService {
+
+    /**
+     * Получает все путешествия анонимного пользователя
+     *
+     * @param anonymousUserId ID анонимного пользователя
+     * @return список путешествий
+     */
+    List<Trip> getAnonymousUserTrips(Long anonymousUserId);
+
+    /**
+     * Получает все TODO листы анонимного пользователя
+     *
+     * @param anonymousUserId ID анонимного пользователя
+     * @return список TODO листов
+     */
+    List<TodoList> getAnonymousUserTodoLists(Long anonymousUserId);
+
+    /**
+     * Переносит владение путешествием от анонимного пользователя к зарегистрированному
+     *
+     * @param anonymousUserId ID анонимного пользователя
+     * @param registeredUserId ID зарегистрированного пользователя
+     * @return количество перенесенных путешествий
+     */
+    int transferTripsOwnership(Long anonymousUserId, Long registeredUserId);
+
+    /**
+     * Переносит владение TODO листами от анонимного пользователя к зарегистрированному
+     *
+     * @param anonymousUserId ID анонимного пользователя
+     * @param registeredUserId ID зарегистрированного пользователя
+     * @return количество перенесенных TODO листов
+     */
+    int transferTodoListsOwnership(Long anonymousUserId, Long registeredUserId);
+} 
